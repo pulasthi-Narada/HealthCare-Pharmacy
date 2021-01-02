c23649c9626a90da2c6cfbe2da76b5735b0b1934
lakna attigala <Lakna0804@gmail.com>
2021-01-02 09:28:39 +0530

    New Interfaces are Added.

--- app/src/main/java/com/healthcarepharmacy/MainActivity.java
+++ app/src/main/java/com/healthcarepharmacy/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_Upload_Prescription,R.id.nav_Categories)
+                R.id.nav_home, R.id.nav_Upload_Prescription,R.id.nav_Categories,R.id.nav_Search_Buy_Medicine,R.id.nav_my_address,R.id.nav_my_Prescription)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
---
+++ app/src/main/java/com/healthcarepharmacy/SearchBuyMedicineActivity.java
@@ -0,0 +1,14 @@
+package com.healthcarepharmacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchBuyMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_buy_medicine);
+    }
+}
