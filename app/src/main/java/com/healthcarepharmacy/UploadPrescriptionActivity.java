efee8d8066b326a374cb43d7b155386dca7ad35f
Pulasthi Nanayakkara <pulasthinarada@hotmail.com>
2021-02-12 15:23:00 +0530

    polished UploadPrescriptionActivity UI.

--- app/src/main/java/com/healthcarepharmacy/UploadPrescriptionActivity.java
+++ app/src/main/java/com/healthcarepharmacy/UploadPrescriptionActivity.java
@@ -3,12 +3,20 @@ package com.healthcarepharmacy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UploadPrescriptionActivity extends AppCompatActivity {
-
+ImageView imgSamplePre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_prescription);
+        imgSamplePre = findViewById(R.id.Upload_prescription_layout_imagsample);
+        //load sample prescription photo to image View
+        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/healthcare-pharmacy-c28f4.appspot.com/o/prescription.png?alt=media&token=d53eee21-6e50-4dfe-921c-68544b6e3838").into(imgSamplePre);
+
+
     }
 }
